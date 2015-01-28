5da7635a5958bd498ba93e39d1e4b37a6fd360e2
Mark Finger <markfinger@gmail.com>
2015-01-29 10:58:46 +1100

    Bug fix to remove the dependency on glamkit-convenient

--- eventtools/conf.py
+++ eventtools/conf.py
@@ -1,4 +1,18 @@
-from convenient.conf import SettingsHandler
-from eventtools import settings as app_settings
+from django.conf import settings as django_settings
 
-settings = SettingsHandler(app_settings)
+
+# Copied out of convenient.conf
+class SettingsHandler(object):
+
+    def __init__(self, app_settings):
+        self.app_settings = app_settings
+
+    def __getattr__(self, attr):
+        try:
+            return getattr(django_settings, attr)
+        except AttributeError:
+            return getattr(self.app_settings, attr)
+
+
+from eventtools import settings as eventtools_settings
+settings = SettingsHandler(eventtools_settings)
